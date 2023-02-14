b1c35cac04344c6ee8fe994ad3a7e290211abe26
alabeeb <alabeeb81@gmail.com>
2023-02-14 18:56:13 +0200

    Initial Commit

---
+++ StoreModule/StoreModule.h
@@ -0,0 +1,18 @@
+//
+//  StoreModule.h
+//  StoreModule
+//
+//  Created by Ahmad Labeeb on 14/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoreModule.
+FOUNDATION_EXPORT double StoreModuleVersionNumber;
+
+//! Project version string for StoreModule.
+FOUNDATION_EXPORT const unsigned char StoreModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreModule/PublicHeader.h>
+
+

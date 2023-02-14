8742533117a1ede4593cb7f1fa7ca907c961e628
alabeeb <alabeeb81@gmail.com>
2023-02-14 21:19:39 +0200

    All modules commit

---
+++ StoreModule/Scenes/StoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StoreViewController.swift
+//  StoreModule
+//
+//  Created by Ahmad Labeeb on 14/02/2023.
+//
+
+import UIKit
+
+public class StoreViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
